2c70be9fd7c0bfdb46abd9899e3b227a4c3d9b4b
Abdul Ghani <azoozy@live.co.uk>
2024-06-21 17:02:57 +0100

    basic concurrent map

---
+++ include/concurrentMap.h
@@ -0,0 +1,72 @@
+#ifndef CONCURRENTMAP_H
+#define CONCURRENTMAP_H
+
+// *Very* basic, just uses a R/W mutex to protect the map
+#include <mutex>
+#include <optional>
+#include <shared_mutex>
+#include <unordered_map>
+#include <utility>
+#include <concepts>
+
+namespace MyServer::Utils {
+
+template <typename T, typename K, typename V>
+concept MapLike = requires(T candidate, K key, V value) {
+  typename T::value_type;
+  typename T::iterator;
+
+  //users can iterate over the map, and we want to use a read-lock for this, so the iteration should be const
+  //probably this isn't enough and the user can cause race conditions if they try
+  // requires requires (T::iterator it) {
+    // std::same_as<typename T::const_iterator, decltype(std::make_const_iterator(it))>;
+  // };
+
+  { candidate.find(key) } -> std::same_as<typename T::iterator>;
+  { candidate.end() } -> std::same_as<typename T::iterator>;
+
+  { candidate.cbegin() } -> std::same_as<typename T::const_iterator>;
+  { candidate.cend() } -> std::same_as<typename T::const_iterator>;
+
+  { candidate.insert(std::make_pair(key, value)) } -> std::same_as<std::pair<typename T::iterator, bool>>;
+  { candidate.erase(key) } -> std::same_as<std::size_t>;
+};
+
+template <typename K, typename V, MapLike<K, V> MapType = std::unordered_map<K,V>>
+class ConcurrentMap 
+{
+private:
+  MapType map;
+  mutable std::shared_mutex rwLock;
+
+public:
+  std::optional<V> get(const K& key) const {
+    std::shared_lock<std::shared_mutex> lock(rwLock);
+    auto it = map.find(key);
+    if (it == map.end()) return {};
+    return *it;
+  }
+
+  bool insert(std::pair<K, V> insertion) {
+    std::unique_lock<std::shared_mutex> lock(rwLock);
+    return map.insert(insertion).second;
+  }
+
+  size_t erase(std::pair<K, V> insertion) {
+    std::unique_lock<std::shared_mutex> lock(rwLock);
+    return map.erase(insertion);
+  }
+
+  //todo check pair constness
+  template <typename FuncType>
+  requires std::invocable<FuncType, const std::pair<K,V>&>
+  void forEach(FuncType&& fun) {
+    for (auto it = map.cbegin(); it != map.cend(); ++it) {
+      fun(*it);
+    }
+  }
+};
+
+}
+#endif
+
--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include "server.h"
+#include "concurrentMap.h"
 
 int main()
 {
-  MyServer::Server server {};
+  using namespace MyServer;
+
+  Server server {};
 
   server.registerHandler(
-    "/echo", MyServer::Request::Method::POST,
-    [](MyServer::Request& request) -> MyServer::Response  {
-      // std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    "/echo", Request::Method::POST,
+    [](Request& request) -> Response  {
       return {
-        .statusCode = MyServer::Response::StatusCode::OK,
+        .statusCode = Response::StatusCode::OK,
         .body = std::string("Server replies: ") + request.body
       };
   });
+
+  Utils::ConcurrentMap<std::string, std::string> todoDatabase {};
+  todoDatabase.insert({"key", "value1"});
+  todoDatabase.insert({"key2", "value2"});
+
+  server.registerHandler(
+    "/list", Request::Method::GET,
+    [&todoDatabase](Request&) -> Response {
+      std::string response {""};
+      todoDatabase.forEach([&response](const std::pair<std::string, std::string>& todo) {
+        response += todo.first + " " + todo.second + "\n";
+      });
+      return {
+        .statusCode = Response::StatusCode::OK,
+        .body = std::move(response)
+      };
+    }
+  );
 
   server.go();
   return 0;
